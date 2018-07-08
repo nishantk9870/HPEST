97b7da4dbdb956d8a5ee213ad3509e9aecd29a92
nishantk <Nishant_Kondapaneni@skillsoft.com>
2018-07-08 23:19:14 +0530

    home page changes with new slider

--- HPEST/www/js/controllers.js
+++ HPEST/www/js/controllers.js
@@ -139,6 +139,34 @@ angular.module('starter.controllers', [])
     }
     setupSlider();
 
+//new slider test code to check how its working . 
+function showBanner(index) {
+  var oldElm = document.querySelector('.slider ion-slide.slider-slide.current');
+  var q = '.slider ion-slide.slider-slide[data-index="' + index + '"]';
+  var elm = document.querySelector(q);
+
+  console.log("Show banner " + index);
+  
+  // Remove class "current"
+  if (null !== oldElm) {
+    oldElm.classList.remove("current");
+  }
+
+  // Add class "current" to current slide
+  if (null !== elm) {
+    elm.classList.add("current");
+  }
+}
+
+$scope.activeSlide = 0;
+
+setTimeout(function() {
+  showBanner($scope.activeSlide);
+}, 100);
+
+$scope.slideChanged = showBanner;
+
+
     $scope.area = [{
       id:0,
       text: 'AREA'
